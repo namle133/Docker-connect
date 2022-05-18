99ea2dc4a621c3ac0c4a545d367ad1709537b367
namle133 <leon.lethanhnam@gmail.com>
2022-05-18 11:04:09 +0700

    Connect postgres

--- main.go
+++ main.go
@@ -3,43 +3,20 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"time"
-
 	_ "github.com/lib/pq"
+	"log"
 )
-
-// Get this package if it's missing.
-// go get -u github.com/lib/pq
-// If you encounter problems like I did with a newer version of Go. Run the following:
-// GO111MODULE="off" go get -u github.com/lib/pq
-// Restart IDE
 
 func main() {
 	fmt.Println("connecting")
 	// these details match the docker-compose.yml file.
-	postgresInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"postgres", 5432, "user", "mypassword", "user")
+	postgresInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+		"localhost", 5432, "postgres", "Namle311", "user")
 	db, err := sql.Open("postgres", postgresInfo)
 	if err != nil {
-		panic(err)
+		log.Fatal(fmt.Sprintf("Unable to connect: %v", err))
 	}
 	defer db.Close()
+	log.Println("Connected to database")
 
-	start := time.Now()
-	for db.Ping() != nil {
-		if start.After(start.Add(10 * time.Second)) {
-			fmt.Println("failed to connect after 10 secs.")
-			break
-		}
-	}
-	fmt.Println("connected:", db.Ping() == nil)
-	_, err = db.Exec(`DROP TABLE IF EXISTS COMPANY;`)
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec(`CREATE TABLE COMPANY (ID INT PRIMARY KEY NOT NULL, NAME text);`)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("table company is created")
 }
